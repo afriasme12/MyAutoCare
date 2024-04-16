8752859feba7c10f304d4acab5d9400d5198a8d4
afriasme12 <afriasme@students.kennesaw.edu>
2024-04-15 22:54:08 -0400

    Updated maintenance.py
    Before, since the "maintenance" table was already created, it would give me an error, so I put in a code that made the program look for the table. I also made the first input choices the user will see.

--- maintenance.py
+++ maintenance.py
@@ -4,12 +4,22 @@ conn = sqlite3.connect('automtc.ed')
 
 c=conn.cursor()
 
-c.execute("""CREATE TABLE maintenance (
-          vehicle text,
-          service_type text,
-          service_date text
-          )""")
+sql_query = """SELECT name FROM sqlite_master WHERE type='table';"""
+c.execute(sql_query)
+results = c.fetchall()
+results_list = [item[0] for item in results]
+for x in results_list:
+    if 'maintenance' in results_list:
+        pass
+    else:
+        c.execute("""CREATE TABLE maintenance (
+                vehicle text,
+                service_type text,
+                service_date text
+                )""")
 
 conn.commit()
 
 conn.close()
+
+autointro = input ("Hello and welcome to MyAutoCare! How may we assist you today?\n 1 = View current maintenance page\n 2 = Update maintenance page\n 3 = Recieve maintenance recommendations\n 4 = Exit\n")
